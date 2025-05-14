95fa60c5e92f9e2766d373dde274b5e26e0ce93f
dmitrymashkalo <dmitrymashkalo@gmail.com>
2025-05-14 13:13:39 +0200

    Add step 2, displays flavors for selected brand

--- src/routers/catalog.py
+++ src/routers/catalog.py
@@ -3,10 +3,9 @@ from aiogram import Router
 from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from src.utils.tools import safe_edit
 from src.utils.text_loader import texts
-from src.config import API_GET_BRANDS
+from src.config import API_GET_BRANDS, API_GET_FLAVORS_BY_BRAND
 
 catalog_router = Router()
-API_URL = API_GET_BRANDS
 
 # step one: select brand with flavors [not empty]
 @catalog_router.callback_query(lambda c: c.data == "catalog")
@@ -24,7 +23,9 @@ async def catalog_brands(callback: CallbackQuery):
 
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text=brand["name"], callback_data=f"brand_{brand['brand_id']}")]
+            [InlineKeyboardButton(
+                text=brand["name"],
+                callback_data=f"brand_{brand['brand_id']}|{brand['name']}")]
             for brand in brands
         ] + [[InlineKeyboardButton(text=texts.get("back_button"), callback_data="back_to_menu")]]
     )
@@ -32,6 +33,41 @@ async def catalog_brands(callback: CallbackQuery):
     await safe_edit(
         callback=callback,
         text=texts.get("brands.select_brand"),
+        reply_markup=keyboard
+    )
+
+    await callback.answer()
+
+
+#step two: get all flavors for selected brand [not empty]
+@catalog_router.callback_query(lambda c: c.data.startswith("brand_"))
+async def brand_flavors(callback: CallbackQuery):
+    raw_data = callback.data.removeprefix("brand_")
+    brand_id, brand_name = raw_data.split("|", 1)
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{API_GET_FLAVORS_BY_BRAND}{brand_id}") as response:
+                response.raise_for_status()
+                data = await response.json()
+                flavors = data.get("flavors", [])
+    except Exception as e:
+        await callback.message.answer(f"Error Message: {e}")
+        await callback.answer()
+        return
+
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(
+                text=f"{flavor["name"]} — {flavor["weight"]}г — {flavor["price"]}zł",
+                callback_data=f"flavor_{flavor['flavor_id']}")]
+            for flavor in flavors
+        ] + [[InlineKeyboardButton(text=texts.get("back_button"), callback_data="catalog")]]
+    )
+
+    await safe_edit(
+        callback=callback,
+        text=texts.get("flavors.select_flavor") + f"{brand_name}:",
         reply_markup=keyboard
     )
 
