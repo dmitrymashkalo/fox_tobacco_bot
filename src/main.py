ae3472950e83fc41e4310b6a7eba4d4213d23374
dmitrymashkalo <dmitrymashkalo@gmail.com>
2025-05-24 01:08:33 +0200

    fix imports

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
-from routers import routers
-from config import BOT_TOKEN
+from src.routers import routers
+from src.config import BOT_TOKEN
 
 
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
